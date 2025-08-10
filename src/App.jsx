9ae2fb2c3d8068d4c5721913c60f992361833184
Krvien16 <kriven.govender07@gmail.com>
2025-08-10 13:19:36 +0200

    compeleted

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
       <section className="padding"><SpecialOffer/></section>
       <section className="padding bg-pale-blue "><CustomerReviews/></section>
       <section className="padding sm:py-32 py-16 w-full"><Subscribe/></section>
-      <section className="padding  padding-x padding-t pb-8 text-  ">
+      <section className="padding  padding-x padding-t pb-8 bg-black  ">
        <Footer/>
        
       </section>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -18,12 +18,12 @@ const Hero = () => {
         <p className="text-xl font-montserrat text-coral-red">
           Our Summer Collection
         </p>
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
-          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold ">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10 ">
             The New Arrival
           </span>
           <br />
-          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
+          <span className="text-coral-red inline-block mt-3 ">Nike</span> Shoes
         </h1>
         <p className="font-montserrat text-slate-gray  text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
           Discover stylish Nike arrivals, qaulity comfort, and innovation for
